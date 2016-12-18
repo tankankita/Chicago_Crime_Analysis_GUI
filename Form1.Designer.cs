252330dd8f9c76eb997d14fc41422026231b0f65
Miss  Tank <tankankita02@gmail.com>
2016-12-18 21:44:49 +0530

    GUI_Crime_Data

---
+++ Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace ChicagoCrime
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+            this.cmdByYear = new System.Windows.Forms.Button();
+            this.graphPanel = new System.Windows.Forms.Panel();
+            this.txtFilename = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cmdByYear
+            // 
+            this.cmdByYear.Location = new System.Drawing.Point(12, 12);
+            this.cmdByYear.Name = "cmdByYear";
+            this.cmdByYear.Size = new System.Drawing.Size(95, 62);
+            this.cmdByYear.TabIndex = 0;
+            this.cmdByYear.Text = "By Year";
+            this.cmdByYear.UseVisualStyleBackColor = true;
+            this.cmdByYear.Click += new System.EventHandler(this.cmdByYear_Click);
+            // 
+            // graphPanel
+            // 
+            this.graphPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.graphPanel.BackColor = System.Drawing.Color.White;
+            this.graphPanel.Location = new System.Drawing.Point(123, 12);
+            this.graphPanel.Name = "graphPanel";
+            this.graphPanel.Size = new System.Drawing.Size(1179, 749);
+            this.graphPanel.TabIndex = 1;
+            // 
+            // txtFilename
+            // 
+            this.txtFilename.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtFilename.BackColor = System.Drawing.SystemColors.Control;
+            this.txtFilename.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFilename.Location = new System.Drawing.Point(123, 769);
+            this.txtFilename.Name = "txtFilename";
+            this.txtFilename.Size = new System.Drawing.Size(1179, 44);
+            this.txtFilename.TabIndex = 2;
+            this.txtFilename.Text = "Crimes-2013-2015.csv";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 130);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 99);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Arrest%";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.byarrest_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 280);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(105, 79);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "By Crime";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(7, 380);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 51);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 480);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(95, 81);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "By Area";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 579);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 51);
+            this.textBox2.TabIndex = 0;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 649);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(95, 55);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "Chicago";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(22F, 44F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1314, 803);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtFilename);
+            this.Controls.Add(this.graphPanel);
+            this.Controls.Add(this.cmdByYear);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Chicago Crime Analysis";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Button cmdByYear;
+    private System.Windows.Forms.Panel graphPanel;
+    private System.Windows.Forms.TextBox txtFilename;
+        private System.Windows.Forms.Button button1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Windows.Forms;
+
+
+namespace ChicagoCrime
+{
+  public partial class Form1 : Form
+  {
+     public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private bool doesFileExist(string filename)
+    {
+      if (!System.IO.File.Exists(filename))
+      {
+        string msg = string.Format("Input file not found: '{0}'",
+          filename);
+
+        MessageBox.Show(msg);
+        return false;
+      }
+      return true;
+    }
+
+    private void clearForm()
+    {
+      if (this.graphPanel.Controls.Count > 0)
+      {
+        this.graphPanel.Controls.RemoveAt(0);
+        this.graphPanel.Refresh();
+      }
+    }
+
+    private void cmdByYear_Click(object sender, EventArgs e)
+    {
+      string filename = this.txtFilename.Text;
+      if (!doesFileExist(filename))
+        return;
+      clearForm();
+      this.Cursor = Cursors.WaitCursor;
+      var chart = FSAnalysis.CrimesByYear(filename);
+      this.Cursor = Cursors.Default;
+      this.graphPanel.Controls.Add(chart);
+            this.graphPanel.Refresh();
+        }
+
+     private void byarrest_Click(object sender, EventArgs e)
+        {
+            string filename = this.txtFilename.Text;
+            if (!doesFileExist(filename))
+                return;
+            clearForm();
+            this.Cursor = Cursors.WaitCursor;
+            var chart2 = FSAnalysis.CrimesByArrest(filename);
+            this.Cursor = Cursors.Default;
+            this.graphPanel.Controls.Add(chart2);
+            this.graphPanel.Refresh();
+    }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {     
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            String IUCR = this.textBox1.Text;
+            string filename = this.txtFilename.Text;
+            if (!doesFileExist(filename))
+                return;
+            clearForm();
+            this.Cursor = Cursors.WaitCursor;
+            var chart2 = FSAnalysis.CrimesByCrime(filename, IUCR);
+            this.Cursor = Cursors.Default;
+            this.graphPanel.Controls.Add(chart2);
+            this.graphPanel.Refresh();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            String area = this.textBox2.Text;
+            string filename = this.txtFilename.Text;
+            if (!doesFileExist(filename))
+                return;
+            clearForm();
+            this.Cursor = Cursors.WaitCursor;
+            var chart2 = FSAnalysis.CrimesByArea(filename, area);
+            this.Cursor = Cursors.Default;
+            this.graphPanel.Controls.Add(chart2);
+            this.graphPanel.Refresh();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string filename = this.txtFilename.Text;
+            if (!doesFileExist(filename))
+                return;
+            clearForm();
+            this.Cursor = Cursors.WaitCursor;
+            var chart2 = FSAnalysis.CrimesByChicago(filename);
+            this.Cursor = Cursors.Default;
+            this.graphPanel.Controls.Add(chart2);
+            this.graphPanel.Refresh();
+        }
+    }
+}
